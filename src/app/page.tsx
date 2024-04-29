0242881f3561d5d5c493a256061a2f1abd3aba95
Rikin Marfatia <rikinm10@gmail.com>
2024-04-29 14:47:53 -0700

    Added Logo + Title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import { Unbounded} from "next/font/google";
+
+const unbounded = Unbounded({
+   subsets: ["latin"],
+})
+
+const size = 44
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Supergooy in Next JS?
+    <main className="flex flex-col min-h-screen items-center text-black">
+      <section className="flex flex-row justify-center items-center gap-2 mt-8">
+        <Image src="/sg-icon.svg" alt="supergooey icon" width={size} height={size}/>
+        <h3 className={`text-3xl font-bold`}>Supergooey</h3>
+      </section>
+      <p>by <span className="font-semibold">Rikin</span></p>
     </main>
   );
 }
