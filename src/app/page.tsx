672fb3b19de6832baf92a7360f68bfaa3f3dedc8
Rikin Marfatia <rikinm10@gmail.com>
2024-05-01 16:13:14 -0700

    Added a lil seperator implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,14 @@ export default function HomePage() {
       </section>
       <section className="w-full flex flex-col gap-4">
         <h1 className="w-full text-left text-3xl">Building Android apps that look and feel <strong>Magical</strong></h1>
-        <p className="w-full text-left text-lg text-gray-500 font-normal">Tired of hearing <em>“Android app when???”</em> Let’s create a
+        <p className="w-full text-left text-lg text-gray-500 font-normal leading-tight">Tired of hearing <em>“Android app when???”</em> Let’s create a
           visually stunning Android experience together at a <span className="text-white font-semibold">fraction</span> of the cost of a full-time developer.</p>
         <div className="flex flex-row gap-3 items-center">
           <a className="no-underline bg-white rounded text-black font-medium p-2 pl-4 pr-4" href="">Let's build</a>
           <a href="">Schedule a call</a>
         </div>
       </section>
+      <div className="w-3/4 m-auto block border-[1px] border-white" />
     </main>
   );
 }
